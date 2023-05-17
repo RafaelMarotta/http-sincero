ca005c417f7e8c4c2d59cb42ab603ad0da905e69
Marotta <rafaelmarottag@gmail.com>
2023-05-17 02:09:11 -0300

    Add status

--- server/app.js
+++ server/app.js
@@ -10,14 +10,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, '../public')));
 
+const status = require('../public/status.json');
+
 app.use('/api/v1/codes/:code', (request, response) => {
   let code = request.params.code
-  let res = {
-    code: code,
-    status: 'Ok',
-    message: 'Deu Brasill!'
+  console.log(status)
+  let res = status.filter(e => e.code == code)
+  if (res.size == 0) {
+    response.sendStatus(404);
   }
-  response.send(res);
+  response.send(res[0]);
 });
 
 module.exports = app;
